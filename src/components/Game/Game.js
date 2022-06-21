8b845e8043437ebbfc293fc9b57f99c14cbe2848
Gardego5 <garrett.davis.7777@gmail.com>
2022-06-20 20:20:41 -0700

    Calculating a hash for neighboring cells

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,9 +4,6 @@ import { Display, Scheduler } from 'rot-js';
 import tileSetImage from '../../media/1bit_2x.png';
 
 class Game {
-  display = null;
-  engine = null;
-  map = {};
   options = {
     layout: 'tile',
     bg: "#472d3c",
@@ -14,10 +11,23 @@ class Game {
     tileHeight: 32,
     tileSet: Object.assign(document.createElement('img'), { src: tileSetImage }),
     _tileMap: {
-        "@": [25, 0],
-        ".": [4, 0],
-        "P": [27, 0],
-        "*": [8, 6],
+      "@": [25, 0],
+      ".": [4, 0],
+      "P": [27, 0],
+      "*": [8, 6],
+      "w_N": [19, 2],
+      "w_NE": [19, 3],
+      "w_E": [18, 1],
+      "w_SE": [19, 4],
+      "w_S": [19, 0],
+      "w_SW": [18, 4],
+      "w_W": [20, 1],
+      "w_NW": [18, 3],
+      "c_NE": [18, 2],
+      "c_SE": [18, 0],
+      "c_SW": [20, 0],
+      "c_NW": [20, 2],
+      "w_X": [0, 13],
     },
     get tileMap() {
       let stretchedTileMap = {};
@@ -34,6 +44,9 @@ class Game {
     width: 30,
     height: 20,
   };
+  display = null;
+  engine = null;
+  map = {};
 
   constructor() {
     this.display = new Display(this.options);
@@ -43,6 +56,25 @@ class Game {
     const attachPoint = document.getElementById('game-display');
 
     attachPoint.appendChild(this.display.getContainer());
+    this.display.draw(5, 5, 'w_N')
+  }
+
+  neighborHash(location) {
+    const x = location[0];
+    const y = location[1];
+    let hash = 0;
+    const places = [
+      [x - 1, y - 1], [x    , y - 1], [x + 1, y - 1],
+      [x - 1, y    ], [x    , y    ], [x + 1, y    ],
+      [x - 1, y + 1], [x    , y + 1], [x + 1, y + 1],
+    ]
+
+    for (let placeIndex in places) {
+      const digit = Math.pow(2, placeIndex) * (this.map[places[placeIndex]] === "floor");
+      hash += digit;
+    }
+
+    return hash;
   }
 }
 
